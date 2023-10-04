c0e45b195acf21552ba8c3b0e043f817e38c1026
AhmidouA <ahmedakkioui75@gmail.com>
2023-10-04 17:49:38 +0200

    Post Model Done

---
+++ models/post.model.js
@@ -0,0 +1,41 @@
+const mongoose = require ('mongoose');
+
+const PostSchema = new mongoose.Schema(
+    {
+        posterId: {
+            type: String,
+            required: true,     
+        },
+        message: {
+            type: String,
+            trim: true,
+            maxLength: 500,
+        },
+        picture: {
+            type: String,       
+        },
+        video: {
+            type: String,
+        },
+        likers: {
+            type: [String],
+            required: true,
+        },
+        comment : {
+            type: [
+                {
+                    commnenterId: String,
+                    commnenterPseudo: String,
+                    text: String,
+                    timestamps: Number,
+                }
+            ],
+            required: true
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model('posts', PostSchema);
