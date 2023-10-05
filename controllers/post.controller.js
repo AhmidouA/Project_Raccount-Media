08e33c3b0847c1c22a8ee2394a06871ba0ba709b
AhmidouA <ahmedakkioui75@gmail.com>
2023-10-05 13:48:42 +0200

    postController: likePost

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -62,7 +62,7 @@ const postController = {
           console.error("Update error : " + err);
           res.status(400).json({ message: err.message });
         }
-      },
+    },
       
 
     async deletePost (req, res) {
@@ -78,13 +78,48 @@ const postController = {
               return res.status(400).json({ message: "Erreur lors de la suppression du post" });
             }
         
-            res.status(200).json({message: "Supprimé avec succès"});
+            return res.status(200).json({message: "Supprimé avec succès"});
           } catch (err) {
             console.error("Delete error : " + err);
             res.status(400).json({ message: err.message });
           }
+    },
 
-    }, 
+    async likePost  (req, res)  {
+        console.log("req.params", req.params);
+        console.log("req.body", req.body);
+        if (!ObjectID.isValid(req.params.id)) {
+          return res.status(400).send("ID inconnu : " + req.params.id);
+        }
+      
+        try {
+          const updatedPost = await PostModel.findByIdAndUpdate(req.params.id,{$addToSet: { likers: req.body.id }},{ new: true });
+          if (!updatedPost) {
+            return res.status(400).json({message: "Erreur lors de l'ajout du likers de post"});
+          }
+      
+          const updatedUser = await UserModel.findByIdAndUpdate(req.body.id,{$addToSet: { likes: req.params.id }},{ new: true });
+          if (!updatedUser) {
+            return res.status(400).json({message: "Erreur lors du like de post"});
+          }
+          return res.status(201).json({updatedUser, updatedPost}); // Vous pouvez choisir de renvoyer updatedPost ou updatedUser, selon vos besoins.
+      
+        } catch (err) {
+            console.error("Delete error : " + err);
+            res.status(400).json({ message: err.message });
+        }
+      },
+      
+
+    async unLikePost (req, res) {
+        console.log("req.params", req.params);
+        console.log("req.body.idToLike", req.body);
+    
+        // Vérification si l'ID de l'utilisateur existe et si l'utilisateur que vous voulez suivre existe aussi
+        if (!ObjectID.isValid(req.params.id) || !ObjectID.isValid(req.body.idToLike)) {
+            return res.status(400).json({ message: 'ID inconnu' });
+        }
+    },
 
 };
 
--- routes/post.route.js
+++ routes/post.route.js
@@ -3,10 +3,15 @@ const router = express.Router();
 
 const { userController, authController, postController } = require("../controllers");
 
+// post
 router.get('/', postController.readPost);
 router.post('/', postController.createPost);
 router.put('/:id', postController.updatePost);
 router.delete('/:id', postController.deletePost);
+
+// like post
+router.patch('/like-post/:id', postController.likePost)
+router.patch('/unlike-post/:id', postController.unLikePost)
 
 
 
