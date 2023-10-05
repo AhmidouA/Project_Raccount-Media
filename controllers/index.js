13927ed14cd0c8af48ae0fd516d13b07578279aa
AhmidouA <ahmedakkioui75@gmail.com>
2023-10-05 18:51:24 +0200

    uploadController: addPicture Done

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const userController = require('./user.controller');
 const postController = require('./post.controller');
 const authController = require('./auth.controller');
+const uploadController = require('./upload.controller');
 
 module.exports = {
     userController,
     postController,
     authController,
+    uploadController,
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
+// const multer = require("multer");
+const upload = require('../utils/multerConfig');
 
-const { userController, authController } = require("../controllers");
+const { userController, authController, uploadController } = require("../controllers");
 
 
 // auth
 router.post('/signUp', authController.signUp);
-router.post('/login', authController.login)
-router.get('/logout', authController.logout)
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
 
 
 
@@ -18,8 +20,11 @@ router.get('/:id', userController.getUser);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 // patch c'est pour changer partiellement et non tout (ex: pour un tableau)
-router.patch('/follow/:id', userController.follow)
-router.patch('/unfollow/:id', userController.unfollow)
+router.patch('/follow/:id', userController.follow);
+router.patch('/unfollow/:id', userController.unfollow);
+
+//uplaod
+router.post('/upload', upload.single('file'), uploadController.uploadProfil);
 
 
 module.exports = router;
