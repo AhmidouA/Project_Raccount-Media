7f8ed4e396df3b909bfc78c64fe849cec1da50c2
AhmidouA <ahmedakkioui75@gmail.com>
2023-11-07 19:56:13 +0100

    Adjusted

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(cookieParser());
 const { userRouter, postRouter } = require("./routes");
 
 // PORT
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // jwt
 app.get("*", authMiddleware.checkUser);
